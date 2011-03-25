1cbb54915be3261d26690d63bf6a05797d607237
kevin.j.fox@gmail.com <kevin.j.fox@gmail.com@df914b50-9440-0e78-2984-5a1cb74aa0de>
2011-03-25 05:11:54 +0000

    Building dedicated class for converting data between system layers

--- trunk/webservice/Prototyping/datamaps.py
+++ trunk/webservice/Prototyping/datamaps.py
@@ -13,18 +13,45 @@ class deviceMap():
             Save current map type
         '''
         self.deviceMap = {}
+        self.AvrMap = {
+            'B': {
+                'DDR':None,
+                'PIN':None,
+                'PORT':None,
+                'D_PORT':None
+                  },
+            'C': {
+                'DDR':None,
+                'PIN':None,
+                'PORT':None,
+                'D_PORT':None
+                  },
+            'D': {
+                'DDR':None,
+                'PIN':None,
+                'PORT':None,
+                'D_PORT':None
+                  }
+        }
                
         if (device_type == 'arduino'):
            loadAvr168()
         else:
             return None  
-
+        
             
         
         
     def loadAvr168(self):
         
-        deviceMap['IOMap'] = {
+        self.deviceMap['DDRMap'] = {
+            'B':'10',
+            'C':'20',
+            'D':'30'
+                                    
+        }
+        
+        self.deviceMap['IOMap'] = {
             0: 'D',
             1: 'D',
             2: 'D',
@@ -44,7 +71,7 @@ class deviceMap():
         '''
             Binary values for pins
         '''
-        deviceMap['AddrMap'] = { 
+        self.deviceMap['AddrMap'] = { 
             0:1,
             1:2,
             2:4,
@@ -60,4 +87,49 @@ class deviceMap():
             12:16,
             13:32
         }
+   
+    def updateMap(self, data):
+       '''
+           Update deviceMap dictionary
+       '''
+       
+       toPort = {'1':'B', '2':'C', '3':'D'}
+       toReg = {'0':'DDR', '1':'PORT', '2':'PIN'}
         
+       # Check to see if the input is valid
+       if ((data[0] == '{') and (data[-1] == '}')):
+           print "Have Proper Framing Characters"
+           '''
+               Parse data from controller
+               Example data:
+               {10:3F,11:2,12:2,20:0,21:0,22:0,30:0,31:0,32:3}
+               
+           '''
+           for portRegPair in data[1:-1].split(','):
+               ''' Un-comment for debugging
+                   print "Process Port -> %s" % (portRegPair)
+               '''
+               portRegData = portRegPair.split(':')
+               ''' Un-comment for debugging
+               print "Process Reg -> %s" % (portRegData)
+               print "Type of data %s" % (type(portRegData[0]))
+               print "---High Nibble %s" % (portRegData[0][0])
+               print "----Low Nibble %s" % (portRegData[0][1])   
+               '''    
+               '''
+                   Convert Hex data to binary
+                   binData - convert hex address into binary string
+               '''
+               binData = bin(int(portRegData[1], 16))[2:]
+               decData = int(portRegData[1], 16)
+
+               self.deviceMap[ toPort[ portRegData[0][0] ] ][ toReg[ portRegData[0][1] ] ] = binData
+               self.deviceMap[ toPort[ portRegData[0][0] ] ][ 'D_PORT' ] = decData
+       else:
+           print "Malformed response from controller"
+           return None
+
+
+
+
+    
