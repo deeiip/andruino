102f9432fc0d9055cbe4c2f84f9ccfa53bccdfe9
b.aryn.jones@gmail.com <b.aryn.jones@gmail.com@df914b50-9440-0e78-2984-5a1cb74aa0de>
2011-03-29 01:26:10 +0000

    Created an Adapter class for the purpose of populating the ListViews. Application is still crashing upon launch.

---
+++ trunk/android/Brittany/src/com/hrc51/csu/andruino/JsonParser.java
@@ -0,0 +1,78 @@
+package com.hrc51.csu.andruino;
+
+import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class JsonParser {
+	private JSONObject jObject;
+	private JSONArray detailArray;
+	private String command;
+	private String response;
+
+	public JsonParser(String jsonString) {
+		try {
+			jObject = new JSONObject(jsonString);
+			command = getValueByName("command");
+	    	response = getValueByName("response");
+	    	detailArray = jObject.getJSONArray("details");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getCommand() {
+		return command;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public ArrayList<String> getDetails() {
+		ArrayList<String> detailList = new ArrayList<String>();
+		try {
+			for (int i = 0; i < Integer.parseInt(response); i++) {
+				detailList.add(detailArray.getJSONObject(i).getString("label").toString());
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return detailList;
+	}
+
+//	public ArrayList<AndruinoObj> getDetails() {
+//		ArrayList<AndruinoObj> detailList = new ArrayList<AndruinoObj>();
+//		try {
+//			for (int i = 0; i < Integer.parseInt(response); i++) {
+//				int did = Integer.parseInt(detailArray.getJSONObject(i).getString("did").toString());
+//				int id = Integer.parseInt(detailArray.getJSONObject(i).getString("id").toString());
+//				String label = detailArray.getJSONObject(i).getString("label").toString();
+//				int ddr = Integer.parseInt(detailArray.getJSONObject(i).getString("ddr").toString());
+//				int pin = Integer.parseInt(detailArray.getJSONObject(i).getString("pin").toString());
+//				int value = Integer.parseInt(detailArray.getJSONObject(i).getString("value").toString());
+//				String ts_value = detailArray.getJSONObject(i).getString("ts_value").toString();
+//				
+//				//System.out.println("did: " + did + " id: " + id + " label: " + label);
+//				AndruinoObj controlElt = new AndruinoObj(did, id, label, ddr, pin, value, ts_value);
+//				detailList.add(controlElt);
+//			}
+//		}
+//		catch (Exception e) {}
+//
+//		return detailList;
+//	}
+	
+	public String getValueByName(String name) {
+		try {
+				return jObject.getString(name);
+			}
+		catch (Exception e) {
+			return e.toString();
+		}
+	}
+}
+
