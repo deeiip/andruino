741ae82aaa8ac23a208d14df404e3e8f6070a5a4
b.aryn.jones <b.aryn.jones@gmail.com>
2011-03-31 02:23:57 +0000

    IOAdapter has been generalized to account for both inputs and outputs.

--- android/Brittany/Andruino/src/com/hrc51/csu/andruino/IOAdapter.java
+++ android/Brittany/Andruino/src/com/hrc51/csu/andruino/IOAdapter.java
@@ -1,12 +1,15 @@
 package com.hrc51.csu.andruino;
 
 import java.util.ArrayList;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class IOAdapter extends ArrayAdapter<AndruinoObj> {
     private ArrayList<AndruinoObj> controls;
@@ -22,21 +25,48 @@ public class IOAdapter extends ArrayAdapter<AndruinoObj> {
     
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-            View v = convertView;
+    	
+    	View v = convertView;
             if (v == null) {
                 LayoutInflater vi = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 v = vi.inflate(textViewResourceId, null);
             }
-            AndruinoObj a = controls.get(position);
-            if (a != null) {
-                    TextView tt = (TextView) v.findViewById(R.id.output_name);
-                    TextView bt = (TextView) v.findViewById(R.id.device_name_out);
-                    if (tt != null) 
-                          tt.setText(a.getLabel());                            
-                    if(bt != null){
-                          bt.setText("Device: ");
+            
+            AndruinoObj andrObj = controls.get(position);
+           
+            switch(textViewResourceId)
+            {
+            case R.layout.indicator_row:
+            	if (andrObj != null) {
+                    TextView indicatorName = (TextView) v.findViewById(R.id.indicator_name);
+                    TextView deviceName = (TextView) v.findViewById(R.id.device_name_ind);
+                    CheckBox notify = (CheckBox)v.findViewById(R.id.notify);
+                    if (indicatorName != null) 
+                          indicatorName.setText(andrObj.getLabel());                            
+                    if(deviceName != null)
+                          deviceName.setText("Device ID: " + andrObj.getId());
+                    if(notify != null)
+                    	//notify.setChecked(andrObj.isNotify());
+                    	notify.setChecked(true);
+	            }
+            	break;
+            
+            case R.layout.output_row:
+	            if (andrObj != null) {
+                    TextView outputName = (TextView) v.findViewById(R.id.output_name);
+                    TextView deviceName = (TextView) v.findViewById(R.id.device_name_out);
+                    ToggleButton tb = (ToggleButton)v.findViewById(R.id.change_state);
+                    if (outputName != null) 
+                          outputName.setText(andrObj.getLabel());                            
+                    if(deviceName != null){
+                          deviceName.setText("Device ID: " + andrObj.getId());
                     }
+                    if(tb != null){
+                    	tb.setChecked(andrObj.getValue() == 1 ? true : false);
+                    }
+	            }
+            	break;
             }
-            return v;
+	        return v;
     }
 }
