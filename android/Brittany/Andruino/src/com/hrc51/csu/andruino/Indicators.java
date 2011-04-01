d734473c9d621ef3bfaf8dc26f87ca9719d00013
b.aryn.jones <b.aryn.jones@gmail.com>
2011-04-01 02:58:36 +0000

    Horizontal swiping enabled and Spinner (dropdown box) now populates with all device names available.

--- android/Brittany/Andruino/src/com/hrc51/csu/andruino/Indicators.java
+++ android/Brittany/Andruino/src/com/hrc51/csu/andruino/Indicators.java
@@ -1,6 +1,7 @@
 package com.hrc51.csu.andruino;
 
 import java.util.ArrayList;
+
 import android.app.ListActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,13 +10,25 @@ import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class Indicators extends ListActivity {
-	private ArrayList<AndruinoObj> indicators = null;
+	private ArrayList<AndruinoObj> allControls;
+	private ArrayList<AndruinoObj> deviceIndicators;
+	private ArrayList<String> deviceNames;
+	private String selectedDevice;
 	private IOAdapter ctrl_adapter; 
     private SharedPreferences settings;
 	private Webduino wc;
+	private Spinner selectDevice;
+	private float initialX, deltaX;
+	
 	
 	/** Called when the activity is first created. */
     @Override
@@ -24,9 +37,29 @@ public class Indicators extends ListActivity {
         setContentView(R.layout.indicators);
         settings = PreferenceManager.getDefaultSharedPreferences(this);
 		wc = new Webduino(settings);
-        indicators = filterControls(wc.read());
+		allControls = wc.read();
+		deviceNames = getDeviceNames(allControls);
+		
+        selectDevice = (Spinner)findViewById(R.id.selectDevice);
+        selectDevice.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, deviceNames));
+        selectDevice.setOnItemSelectedListener(new OnItemSelectedListener()
+        {
+            public void onItemSelected(AdapterView<?> arg0, 
+            View arg1, int arg2, long arg3) 
+            {
+                int index = selectDevice.getSelectedItemPosition();
+                selectedDevice = deviceNames.get(index).toString();
+//                Toast.makeText(getBaseContext(), 
+//                        "You have selected item: " + selectedDevice, 
+//                        Toast.LENGTH_SHORT).show();
+            }
+ 
+            public void onNothingSelected(AdapterView<?> arg0) {}
+        });
         
-        ctrl_adapter = new IOAdapter(this, R.layout.indicator_row, indicators);
+        
+        deviceIndicators = filterControls(allControls, selectedDevice);
+        ctrl_adapter = new IOAdapter(this, R.layout.indicator_row, deviceIndicators);
         setListAdapter(ctrl_adapter);
     }
     
@@ -60,14 +93,51 @@ public class Indicators extends ListActivity {
 		return true;
 	}
 	
-	public ArrayList<AndruinoObj> filterControls(ArrayList<AndruinoObj> controls) {
-		ArrayList<AndruinoObj> indicators = new ArrayList<AndruinoObj>();
+	public boolean onTouchEvent(MotionEvent event) {
+        synchronized (event)  
+        {   
+        	//when user touches the screen  
+        	if(event.getAction() == MotionEvent.ACTION_DOWN)  
+        	{  
+        		deltaX = 0;  
+        		initialX = event.getRawX();  
+        	}  
+  
+        	//when screen is released  
+        	if(event.getAction() == MotionEvent.ACTION_UP)  
+        	{  
+        		deltaX = event.getRawX() - initialX;  
+  
+        		//swiped from left to right  
+        		if(deltaX < 0)   
+        			startActivity(new Intent(this, Outputs.class)); 
+  
+        		return true;  
+                }      
+        }  
+		return true;
+	}
+	
+	public ArrayList<AndruinoObj> filterControls(ArrayList<AndruinoObj> controls, String selectedDevice) {
+		ArrayList<AndruinoObj> deviceIndicators = new ArrayList<AndruinoObj>();
 		
 		for(AndruinoObj obj : controls)
 		{
 			if(obj.getDdr() == 1)
-				indicators.add(obj);
+				deviceIndicators.add(obj);         
 		}
-		return indicators;
+		return deviceIndicators;
+	}
+	
+	public ArrayList<String> getDeviceNames(ArrayList<AndruinoObj> allControls) {
+		ArrayList<String> deviceNames = new ArrayList<String>();
+		
+		for(AndruinoObj obj: allControls) /* app is crashing here */
+		{
+			String device = obj.getDevice();
+			if(!deviceNames.contains(device))
+				deviceNames.add(obj.getDevice());
+		}
+		return deviceNames;
 	}
 }
