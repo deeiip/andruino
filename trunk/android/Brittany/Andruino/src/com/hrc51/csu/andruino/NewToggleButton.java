01bf7617ea862646c67f5c37ac67ab7688927f58
b.aryn.jones@gmail.com <b.aryn.jones@gmail.com@df914b50-9440-0e78-2984-5a1cb74aa0de>
2011-04-24 19:52:55 +0000

    New subclass of toggle button created to override toggle method of normal toggle buttons.

---
+++ trunk/android/Brittany/Andruino/src/com/hrc51/csu/andruino/NewToggleButton.java
@@ -0,0 +1,25 @@
+package com.hrc51.csu.andruino;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ToggleButton;
+
+public class NewToggleButton extends ToggleButton {
+	
+	public NewToggleButton(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NewToggleButton(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+	public NewToggleButton(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+	
+	public void toggle() {
+		
+	}
+}
